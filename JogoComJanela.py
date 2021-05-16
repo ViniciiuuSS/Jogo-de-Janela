f35893d542db6a088551526347cec14b5105eba0
Vinicius Guedes <80132730+ViniciiuuSS@users.noreply.github.com>
2021-05-16 16:19:22 -0300

    Update JogoComJanela.py

--- JogoComJanela.py
+++ JogoComJanela.py
@@ -1,12 +1,6 @@
 import pygame
 from random import randint
-from tkinter import *
-from kivy.uix.widget import Widget
-from kivy.app import App
-import keyboard
-import string
-from threading import *
-import simplegui
+from time import sleep
 
 pygame.init()
 
@@ -20,10 +14,14 @@ pos_y_1 = 200
 pos_atual_y = 300
 velocidade = 10
 velocidade_Inimigo = 10
-Moeda = 180
+Moeda = 800
 vida = 100
-Nivel = 9
+Nivel = 0
 xp = 0
+PocaoVida = 0
+PocaoVida_Preco = 100
+PocaoXP = 0
+PocaoXP_Preco = 100
 #-----------------------------------------------------------------------------------------------------------------------
 
 #----------------------------------------------------Armas e preço de armas---------------------------------------------
@@ -33,6 +31,8 @@ Espada_Do_Fin_atri = randint(3,10)
 Espada_Do_Fin_preço = 600
 EspadaDePedra_atri = randint(5,11)
 EspadaDePedra_preço = 200
+
+Arma_atual_num = 0
 #-----------------------------------------------------------------------------------------------------------------------
 
 #----------------------------------------------------imagens------------------------------------------------------------
@@ -59,25 +59,34 @@ FundoLoja = pygame.image.load("FundoLoja.png")
 #-----------------------------------------------------------------------------------------------------------------------
 
 #----------------------------------------------------tempo e money------------------------------------------------------
-font = pygame.font.SysFont("arial black", 30)
+font = pygame.font.SysFont("arial black", 20)
 texto = font.render("Nivel: "+str(Nivel), True, (0,0,0), (46,139,87))
 pos_Nivel = texto.get_rect()
-pos_Nivel = (610,527)
+pos_Nivel = (660,527)
 
-font = pygame.font.SysFont("arial black", 30)
+font = pygame.font.SysFont("arial black", 20)
 moeda_texto = font.render("Money: "+str(Moeda), True, (0,0,0), (255,215,0))
 pos_texto_moeda = texto.get_rect()
-pos_texto_moeda = (610,483)
+pos_texto_moeda = (660,483)
 
-font = pygame.font.SysFont("arial black", 30)
+font = pygame.font.SysFont("arial black", 20)
 Vida_texto = font.render("Vida: "+(str(vida)), True, (0,0,0), (255,69,0))
 pos_texto_Vida = texto.get_rect()
-pos_texto_Vida = (610,443)
+pos_texto_Vida = (660,443)
+
+font = pygame.font.SysFont("arial black", 20)
+PocaoVida_texto = font.render("POCAO: "+(str(PocaoVida)), True, (0,0,0), (255,69,0))
+pos_texto_PocaoVida = texto.get_rect()
+pos_texto_PocaoVida = (589,479)
+
+font = pygame.font.SysFont("arial black", 20)
+PocaoXP_texto = font.render("POCAO: "+(str(PocaoXP)), True, (0,0,0), (255,69,0))
+pos_texto_PocaoXP = texto.get_rect()
+pos_texto_PocaoXP = (589,550)
 #-----------------------------------------------------------------------------------------------------------------------
 
 #----------------------------------------------------Programa rodando---------------------------------------------------
 janela = pygame.display.set_mode ((800,600))
-teclas = list(string.ascii_lowercase)
 
 janela_Aberta = True
 while janela_Aberta:
@@ -86,6 +95,7 @@ while janela_Aberta:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             janela_Aberta = False
+
 
     comandos = pygame.key.get_pressed()
     if comandos[pygame.K_w] and y >= 0:
@@ -98,14 +108,6 @@ while janela_Aberta:
         x += velocidade
 
 
-    def listen(a):
-        while True:
-            keyboard.wait(a)
-            print("- Tecla pressionada: ", a)
-            threads = [Thread(target=listen, kwargs={"tecla": a}) for a in teclas]
-        for thread in threads:
-            Moeda += 10
-            thread.start()
     #COLISAO PARA CADA INIMIGO, QUANTIDADE ATUAL : 2
     #INIMIGO 0
     if ( x + 90 > pos_x and x  < pos_x and y - 40 < pos_y and y + 40 > pos_y): #colisao no inimigo
@@ -119,6 +121,7 @@ while janela_Aberta:
             Nivel += 1
             texto = font.render("Nivel: "+str(Nivel), True, (0,0,0), (46,139,87))
         moeda_texto = font.render("Money: "+(str(Moeda)), True, (0, 0, 0), (255, 215, 0))
+
 
     #INIMIGO 1
     if ( x + 90 > pos_x_1 and x  < pos_x_1 and y - 40 < pos_y_1 and y + 40 > pos_y_1): #colisao no inimigo
@@ -140,6 +143,7 @@ while janela_Aberta:
         pos_y = randint(0, 145)
         vida -= 1
         Vida_texto = font.render("Vida: " + (str(vida)), True, (0, 0, 0), (255, 69, 0))
+
     #INIMIGO 1
     if pos_x_1 <= -180:
         pos_x_1 = randint(1000,1400)
@@ -148,28 +152,64 @@ while janela_Aberta:
         Vida_texto = font.render("Vida: " + (str(vida)), True, (0, 0, 0), (255, 69, 0))
     #FIM QUANTIDADE DE INIMIGOS
 
-
+    PocaoVida_texto = font.render(f"{(str(PocaoVida))}"  , True, (0, 0, 0))
+    PocaoXP_texto = font.render(f"{(str(PocaoXP))}" , True, (0, 0, 0))
     pos_x -= velocidade_Inimigo
     pos_x_1 -= velocidade_Inimigo
 
     janela.blit(fundo,(0,0))
-
-    if Moeda < 200 :
-        Personagem = janela.blit(PersonagemImage, (x, y))
-    if Moeda >= 200 :
-        Personagem = janela.blit(PersonagemComEspada_pedra, (x, y))
-        Moeda -= EspadaDePedra_preço
-    if Moeda >= 400 :
-        Personagem = janela.blit(PersonagemComEspada_diamante, (x, y))
-    if Moeda >= 600 :
-        Personagem = janela.blit(PersonagemComEspada_fin, (x, y))
+    if Arma_atual_num == 0:
+        Arma_atual = janela.blit(PersonagemImage, (x, y))
     janela.blit(Inimigo,(pos_x,pos_y))
     janela.blit(Inimigo1, (pos_x_1,pos_y_1))
     janela.blit(texto, pos_Nivel)
     janela.blit(moeda_texto,pos_texto_moeda)
     janela.blit(Vida_texto,pos_texto_Vida)
     janela.blit(FundoBarraDeNivel, (0,580))
+    janela.blit(PocaoVida_texto, pos_texto_PocaoVida)
+    janela.blit(PocaoXP_texto,pos_texto_PocaoXP)
     #BARRA DE NIVEL
+    comandos = pygame.key.get_pressed()
+    if comandos[pygame.K_v]:
+       if vida <= 99:
+           if PocaoVida > 0:
+                PocaoVida -= 1
+                if vida <= 99:
+                    vida += 30
+    if comandos[pygame.K_TAB]:
+        janela.blit(FundoLoja, (100, 0))
+        if comandos[pygame.K_F1]:
+            if Moeda >= 200:
+                Arma_atual_num = 1
+                velocidade += randint(4, 7)
+                Moeda -= EspadaDeDiamante_preço
+        if comandos[pygame.K_F2]:
+            if Moeda >= 400:
+                Arma_atual_num = 2
+                velocidade += randint(5, 10)
+                Moeda -= Espada_Do_Fin_preço
+        if comandos[pygame.K_F3]:
+            if Moeda >= 600:
+                Arma_atual_num = 3
+                velocidade += randint(2, 5)
+                Moeda -= EspadaDePedra_preço
+        if comandos[pygame.K_F4]:
+            if Moeda >= 100:
+                PocaoVida += 1
+                Moeda -= PocaoVida_Preco
+                PocaoVida_texto = font.render("POCAO: " + (str(PocaoVida)), True, (0, 0, 0), (255, 69, 0))
+        if comandos[pygame.K_F5]:
+            if Moeda >= 100:
+                xp += 1
+                Moeda -= PocaoXP_Preco
+                PocaoXP_texto = font.render("POCAO: " + (str(PocaoXP)), True, (0, 0, 0), (255, 69, 0))
+    if Arma_atual_num == 3:
+        Arma_atual = janela.blit(PersonagemComEspada_pedra, (x, y))
+    if Arma_atual_num == 1:
+        Arma_atual = janela.blit(PersonagemComEspada_diamante, (x, y))
+    if Arma_atual_num == 2:
+        Arma_atual = janela.blit(PersonagemComEspada_fin, (x, y))
+
     if xp == 1:
         janela.blit(BarraDeNivel, (0, 580))
     if xp == 2:
